52d9efd0360777d9c2e68dcdaa0dca36e99fb1a9
Carson Covell <cocarson@umich.edu>
2015-03-30 11:40:19 -0400

    Updated main layout for routing

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,17 @@
 var app = angular.module('finanza', [
 	'ngMaterial',
+	'ngRoute',
 	'ngResource'
 ]).config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
     .primaryPalette('light-blue')
     .accentPalette('red');
 });
+
+app.config(['$routeProvider', 
+	function($routeProvider) {
+		$routeProvider.
+			when('/', {
+				templateUrl: 'views/groups.html',
+			});
+	}]);
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.get('/', function(req, res) {
 
 app.use('/bower', express.static(__dirname + '/bower_components'));
 app.use('/js', express.static(__dirname + '/client/js'));
+app.use('/views', express.static(__dirname + '/client/views'));
 
 app.listen(3000, function() {
 	console.log('App running on port 3000');
